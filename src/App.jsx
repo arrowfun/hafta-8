cf601b52f4b2ecbafdb71c9b9f0b32790602c706
Oğuzhan Garip <oguzhan.garip21@gmail.com>
2024-03-03 17:32:37 +0300

    feat : kart

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,13 @@
 
 function App() {
   return (
-    <div className="App">
-       Merhaba
+    <div className="container">
+       <div className="row">
+          <div className="col-12 col-md-3">
+            <h2>Ürün Adı 1</h2>
+            <img src="http://lipsum.app/id/12/300/200" />
+          </div>
+       </div>
     </div>
   );
 }
